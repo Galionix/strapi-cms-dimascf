be68a624a54186debe9f74b4d88b81880d78ccf0
galionix <parfumeland@ya.ru>
2021-07-20 23:05:12 +0300

    u

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "3.223.221.167"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cfc719e33590e3d0d06b428e1944d8f4'),
+      secret: env("ADMIN_JWT_SECRET", "cfc719e33590e3d0d06b428e1944d8f4"),
     },
   },
 });
